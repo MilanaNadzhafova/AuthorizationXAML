6db83bd946a7c556ce4e053ba4a1e98254436a6e
NadzhafovaMilana <mnadgafova47@gmail.com>
2021-12-04 04:41:04 +0700

    Добавьте файлы проекта.

---
+++ AuthorizationXAML/MainScreen.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AuthorizationXAML
+{
+    /// <summary>
+    /// Логика взаимодействия для MainScreen.xaml
+    /// </summary>
+    public partial class MainScreen : Window
+    {
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void visitor_Click(object sender, RoutedEventArgs e)
+        {
+            new VisitorMenu().Show();
+            this.Close();
+        }
+    }
+}
---
+++ AuthorizationXAML/VisitorMenu.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AuthorizationXAML
+{
+    /// <summary>
+    /// Логика взаимодействия для VisitorMenu.xaml
+    /// </summary>
+    public partial class VisitorMenu : Window
+    {
+        public VisitorMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            new MainScreen().Show();
+            this.Close();
+        }
+    }
+}
